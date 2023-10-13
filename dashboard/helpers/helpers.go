1bd064eac1f9e050879b6d261aae55755cc39bb9
Ishwarya-Selvaraj <77732470+Ishwarya-Selvaraj@users.noreply.github.com>
2023-10-13 18:38:53 +0530

    Update helpers.go

--- dashboard/helpers/helpers.go
+++ dashboard/helpers/helpers.go
@@ -2,14 +2,14 @@ package helpers
 
 // CheckError handler 
 func CheckError(err error) {
-	if err != nil {
-		panic(err)
-	}
+    if err != nil {
+        panic(err)
+    }
 }
 
 // CheckError with custom message
 func CheckErrorWithMessage(err error, message string) {
-	if err != nil {
-		panic(message)
-	}
+    if err != nil {
+        panic(message)
+    }
 }
