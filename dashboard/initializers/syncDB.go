c50116e0efa14ab31b924341e48ed1067b682b2c
Ishwarya-Selvaraj <77732470+Ishwarya-Selvaraj@users.noreply.github.com>
2023-10-13 18:40:41 +0530

    Update syncDB.go

--- dashboard/initializers/syncDB.go
+++ dashboard/initializers/syncDB.go
@@ -3,5 +3,5 @@ package initializers
 import "github.com/rohansingh9001/Neura-Launch-Dashboard/models"
 
 func SyncDatabase() {
-	DB.AutoMigrate(&models.User{})
+    DB.AutoMigrate(&models.User{})
 }
