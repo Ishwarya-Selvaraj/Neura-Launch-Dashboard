2813961ed99317ddbe883426e7436d8631fca4bf
Ishwarya-Selvaraj <77732470+Ishwarya-Selvaraj@users.noreply.github.com>
2023-10-13 18:39:35 +0530

    Update loadDB.go

--- dashboard/initializers/loadDB.go
+++ dashboard/initializers/loadDB.go
@@ -1,25 +1,25 @@
 package initializers
 
 import (
-	"os"
-	"fmt"
-	"strconv"
+    "os"
+    "fmt"
+    "strconv"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectToDb() {
-	var err error
-	port, _ := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
-	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("POSTGRES_HOST"), port, os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
-	DB, err = gorm.Open(postgres.Open(psqlconn), &gorm.Config{})
-	if err != nil {
-		panic("Failed to connect to DB")
-	} else {
-		fmt.Print("Successfully connected to Database!")
-	}
+    var err error
+    port, _ := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+    psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+        os.Getenv("POSTGRES_HOST"), port, os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+    DB, err = gorm.Open(postgres.Open(psqlconn), &gorm.Config{})
+    if err != nil {
+        panic("Failed to connect to DB")
+    } else {
+        fmt.Print("Successfully connected to Database!")
+    }
 }
